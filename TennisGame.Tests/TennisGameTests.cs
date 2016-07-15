c0cb2b32bbb01c6c2362064d898ee3572c0e92e5
sharonrussell <sharon.russell@comparethemarket.com>
2016-07-15 23:14:27 +0100

    Tennis game score calculator with unit tests

---
+++ TennisGame.Tests/TennisGameTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TennisGame.Tests
+{
+	[TestFixture]
+	public class TennisGameTests
+	{
+		[TestCase(15, 0, "15-Love")]
+		[TestCase(0, 0, "Love-Love")]
+		[TestCase(40, 40, "Deuce")]
+		public void ShouldOutputCorrectScore(int score1, int score2, string result){
+			var game = new Game();
+
+			Assert.That(game.Score(score1, score2), Is.EqualTo(result));
+		}
+	}
+}
---
+++ TennisGame/Game.cs
@@ -0,0 +1,27 @@
+﻿namespace TennisGame
+{
+	public class Game
+	{
+		public string Score(int score1, int score2){
+			return IsDeuce(Add(Convert(score1), Convert(score2)));
+		}
+
+		private string IsDeuce(string score){
+			if (score.Equals("40-40"))
+				return "Deuce";
+
+			return score;
+		}
+
+		private string Convert(int score)
+		{
+			return score.ToString().Replace("0", "Love");
+		}
+
+		private string Add(string score1, string score2)
+		{
+			return score1 + "-" + score2;
+		}
+	}
+}
+
